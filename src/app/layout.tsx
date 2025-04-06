7ac8cb122c817461bef5f03408c6fee7c949fb60
Jordan Castiglioni <jordan@jdncst.dev>
2025-04-06 03:33:25 +0200

    setup auto deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'jdncst.dev',
+  title: 'Jordan Castiglioni',
+  description: 'Full Stack Developer',
 }
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+import { ExternalLink } from 'lucide-react'
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <>
       <main className='flex flex-col items-center justify-center min-h-screen py-2 px-2'>
-        <h1 className='text-3xl font-bold mb-4'>jdncst.dev</h1>
-        <p className='text-xl'>Work In Progress</p>
+        <h1 className='text-3xl font-bold'>Jordan Castiglioni</h1>
+        <h2 className='text-2xl font-bold mb-4'>Full Stack Developer</h2>
+        <p className='text-xl mb-4'>Work In Progress</p>
+        <div className='flex flex-col items-center gap-2'>
+          <Link
+            className='text-md flex gap-2 items-center'
+            href='https://www.linkedin.com/in/jordan-castiglioni/'
+            target='blank'
+          >
+            LinkedIn
+            <ExternalLink size={16} />
+          </Link>
+          <Link
+            className='text-md flex gap-2 items-center'
+            href='https://github.com/jdncst-dev/'
+            target='blank'
+          >
+            Github
+            <ExternalLink size={16} />
+          </Link>
+        </div>
       </main>
     </>
   )
